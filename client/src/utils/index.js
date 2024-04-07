5425ad802925775090baf7f166dee664ed2092f1
Vicky Gidvir <vicky.gidvir@pies.io>
2024-04-07 15:29:41 +0530

    server completed

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,12 +1,30 @@
 import axios from 'axios';
 import { SetPosts } from '../redux/postSlice';
+import { apiRequest } from './apirequest';
 
-const API_URL = "https://soaiclmediaapi.onrender.com/";
+const API_URL = "https://social-media-web-psi.vercel.app/";
 
 export const API = axios.create({
   baseURL: API_URL,
   responseType: "json",
 });
+
+export const handleFileUpload = async (uploadFile)=>{
+
+  const formData = new FormData();
+  formData.append("file", uploadFile);
+  formData.append("upload_preset", "socialmedia");
+
+  try{
+    const response = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_ID}/image/upload/`,formData);
+
+    return response.data.secure_url;
+
+  }catch(error){
+
+  }
+
+}
 
 export const apiRequest = async ({ url, token, data, method, userId }) => {  
   try {
@@ -23,6 +41,8 @@ export const apiRequest = async ({ url, token, data, method, userId }) => {
     return { status: err.status, message: err.message };
   }
 };
+
+
 
 
 export const fetchPosts = async (user, dispatch) => {  
@@ -49,23 +69,23 @@ export const deletePost = async (postId) => {
 
 export const getUserInfo = async (user, id) => {  
   console.log("get user info", user , id )
-  // try {
-  //   const url = id === undefined ? "/users/getuser" : "/users/getuser/" + id;
-  //   const res = await apiRequest({
-  //     url: url,
-  //     userId: user?.user?._id,
-  //     method: "GET",
-  //   });
+  try {
+    const url = id === undefined ? "/users/getuser" : "/users/getuser/" + id;
+    const res = await apiRequest({
+      url: url,
+      userId: user?.user?._id,
+      method: "GET",
+    });
 
-  //   if (res.message === "Authentication failed") {
-  //     localStorage.removeItem("user");
-  //     window.alert("User session expired. Login again.");
-  //     window.location.replace("/login");
-  //   }
-  //   return res;
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    if (res.message === "Authentication failed") {
+      localStorage.removeItem("user");
+      window.alert("User session expired. Login again.");
+      window.location.replace("/login");
+    }
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const sendFriendRequest = async (user, id) => {
